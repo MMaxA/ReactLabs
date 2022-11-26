7a4fdb16181a142afa446b5c2e4497451e3c162c
Медведев Максим Александрович <medvedevma@rambler.ru>
2022-11-26 14:46:30 +0500

    First part

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,10 @@
 import React from "react";
-import img1 from '../images/f3.png';
 
 function Todo() {
   return (
     <div className="card">
       <h2>Title</h2>
       <div className="actions">
-        <img src={img1} />
         <button className="btn">Delete</button>
       </div>
     </div>
